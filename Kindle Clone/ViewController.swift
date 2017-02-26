e8e4876ab699a9e46cfd6f87a010f4d985018e4f
Cynthia Strickland <cynthiastrickland001@gmail.com>
2017-02-26 14:55:49 -0800

    TableView functional loading Title and Author of book into cells.

--- Kindle Clone/ViewController.swift
+++ Kindle Clone/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupBooks()
         setupNavBarButtonItems()
         fetchBooks()
         
@@ -91,13 +90,16 @@ class ViewController: UITableViewController {
                 
                 guard let bookDictionaries = json as? [[String:Any]] else { return }
                 
-                for bookDictionary in bookDictionaries {
-                    if let title = bookDictionary["title"] as? String,
-                        let author = bookDictionary["author"] as? String {
-                        
-                        let book = Book(title: title, author: author, pages: [], image: #imageLiteral(resourceName: "steve_jobs.jpg"))
-                        print(book.title)
+                self.books = []
+                    for bookDictionary in bookDictionaries {
+                        if let title = bookDictionary["title"] as? String,
+                            let author = bookDictionary["author"] as? String {
+                            let book = Book(title: title, author: author, pages: [], image: #imageLiteral(resourceName: "steve_jobs.jpg"))
+                            self.books?.append(book)  //specify self due to retain cycles
+                        }
                     }
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()   //Dangerous to execute this on main thread
                 }
             } catch let jsonError {
                 print("Failed to Parse JSON properly", jsonError)
@@ -105,31 +107,31 @@ class ViewController: UITableViewController {
             }).resume()
         }
     }
-    
-    
-    func setupBooks() {
-        let page1 = Page(number: 1, text: "Text for first page")
-        let page2 = Page(number: 2, text: "Text for second page")
-        
-        let pages = [page1, page2]
-        
-        let book = Book(title: "Steve Jobs", author: "Walter Isaacson", pages:pages, image: #imageLiteral(resourceName: "steve_jobs.jpg"))
-        
-        for page in book.pages {
-            print(page.text)
-        }
-        
-        let book2 = Book(title: "Bill Gates Autobiography", author: "Michael Becraft", pages: [
-            
-            Page(number: 1, text: "Text for Page 1"),
-            Page(number: 2, text: "Text for Page 2"),
-            Page(number: 3, text: "Text for Page 3"),
-            Page(number: 4, text: "Text for Page 4"),
-            Page(number: 1, text: "Text for Page 5")
-            
-            ], image: #imageLiteral(resourceName: "bill_gates.jpg"))
-        
-        self.books = [book, book2]
-    }
 }
+    
+//    func setupBooks() {
+//        let page1 = Page(number: 1, text: "Text for first page")
+//        let page2 = Page(number: 2, text: "Text for second page")
+//        
+//        let pages = [page1, page2]
+//        
+//        let book = Book(title: "Steve Jobs", author: "Walter Isaacson", pages:pages, image: #imageLiteral(resourceName: "steve_jobs.jpg"))
+//        
+//        for page in book.pages {
+//            print(page.text)
+//        }
+//        
+//        let book2 = Book(title: "Bill Gates Autobiography", author: "Michael Becraft", pages: [
+//            
+//            Page(number: 1, text: "Text for Page 1"),
+//            Page(number: 2, text: "Text for Page 2"),
+//            Page(number: 3, text: "Text for Page 3"),
+//            Page(number: 4, text: "Text for Page 4"),
+//            Page(number: 1, text: "Text for Page 5")
+//            
+//            ], image: #imageLiteral(resourceName: "bill_gates.jpg"))
+//        
+//        self.books = [book, book2]
+//    }
+
 
